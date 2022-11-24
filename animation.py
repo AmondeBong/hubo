54e3d88006124a13bfb8acafa610558918373342
AmondeBong <oms1218ju@gmail.com>
2022-11-24 15:39:24 +0900

    22.11.24 python hubo

--- animation.py
+++ animation.py
@@ -1,13 +1,42 @@
 from cs1graphics import *
 from time import *
+import math
+import random
 
+# rotate(n) 시계 방향으로 n도 회전 시킴
+# scale(n) n배 커짐
+# flip(n) 축을  n도 회전시켜서 기준으로 접힘
 paper = Canvas()
 
 paper.setBackgroundColor('powderBlue')
 paper.setWidth(500)
-paper.setHeight(300)
+paper.setHeight(200)
 paper.setTitle('draw')
 
+
+def animate_sunrise(sun):
+    w = paper.getWidth()
+    h = paper.getHeight()
+    r = sun.getRadius()
+    x0 = w / 2.0
+    y0 = h + r
+    xradius = w / 2.0 - r
+    yradius = h
+    for angle in range(181):  # 0 ~ 180
+        rad = (angle/180.0) * math.pi
+        x = x0 - xradius * math.cos(rad)
+        y = y0 - yradius * math.sin(rad)
+        sun.moveTo(x, y)
+
+
+ground = Ellipse(700, 100, Point(300, 200))
+ground.setFillColor('green')
+paper.add(ground)
+
+sun = Circle(50, Point(400, 100))
+sun.setFillColor('orange')
+sun.setDepth(60)
+paper.add(sun)
 
 roof = Polygon(Point(105, 105), Point(175, 105),
                Point(170, 85), Point(110, 85))
@@ -18,31 +47,98 @@ facade = Square(60, Point(140, 130))
 facade.setFillColor('gray')
 paper.add(facade)
 
-car = Layer()
-tire1 = Circle(10, Point(-20, -10))
-tire1.setFillColor('darkGray')
-car.add(tire1)
+cat = Layer()
+leg1 = Rectangle(10, 10, Point(-20, -10))
+leg1.setFillColor('brown')
+cat.add(leg1)
 
-tire2 = Circle(10, Point(20, -10))
-tire2.setFillColor('darkGray')
-car.add(tire2)
+leg2 = Rectangle(10, 10, Point(20, -10))
+leg2.setFillColor('brown')
+cat.add(leg2)
 
-body = Rectangle(70, 30, Point(0, -25))
-body.setFillColor('khaki')
-car.add(body)
+body = Rectangle(60, 30, Point(0, -30))
+body.setFillColor('brown')
+cat.add(body)
 
-car.moveTo(110, 180)
-car.setDepth(20)
-paper.add(car)
+head = Circle(20, Point(-30, -50))
+head.setFillColor('brown')
+cat.add(head)
 
-timeDelay = 1
-sleep(timeDelay)
+eyes1 = Circle(3, Point(-21, -53))
+eyes1.setFillColor('black')
+cat.add(eyes1)
+eyes2 = Circle(3, Point(-41, -53))
+eyes2.setFillColor('black')
+cat.add(eyes2)
+nose = Ellipse(10, 5, Point(-30, -43))
+nose.setFillColor('black')
+cat.add(nose)
 
-car.move(-10, 0)
-sleep(timeDelay)
-car.move(-30, 0)
-sleep(timeDelay)
-car.move(-60, 0)
-sleep(timeDelay)
-car.move(-100, 0)
-sleep(timeDelay)
+
+ear1 = Polygon(Point(-15, -80), Point(-25, -65), Point(-15, -60))
+ear1.setFillColor('brown')
+cat.add(ear1)
+
+ear2 = Polygon(Point(-45, -80), Point(-35, -65), Point(-45, -60))
+ear2.setFillColor('brown')
+cat.add(ear2)
+
+
+giraffe = Layer()
+
+leg1 = Rectangle(10, 70, Point(400, 150))
+leg1.setFillColor('yellow')
+giraffe.add(leg1)
+leg2 = Rectangle(10, 70, Point(320, 150))
+leg2.setFillColor('yellow')
+giraffe.add(leg2)
+body = Rectangle(100, 30, Point(360, 130))
+body.setFillColor('yellow')
+giraffe.add(body)
+neck = Rectangle(20, 70, Point(400, 100))
+neck.setFillColor('yellow')
+giraffe.add(neck)
+head = Polygon(Point(400, 50), Point(450, 110), Point(400, 100))
+head.setFillColor('yellow')
+giraffe.add(head)
+ear1 = Rectangle(8, 15, Point(410, 50))
+ear1.setFillColor('brown')
+giraffe.add(ear1)
+ear2 = Rectangle(8, 15, Point(400, 50))
+ear2.setFillColor('brown')
+giraffe.add(ear2)
+eyes2 = Circle(10, Point(415, 70))
+eyes2.setFillColor('white')
+giraffe.add(eyes2)
+eyes1 = Circle(3, Point(415, 70))
+eyes1.setFillColor('black')
+giraffe.add(eyes1)
+
+pattern = [None for _ in range(8)]
+
+for i in range(8):
+    num1 = random.randrange(320, 400)
+    num2 = random.randrange(120, 140)
+    pattern[i] = Square(6, Point(num1, num2))
+    pattern[i].setFillColor('brown')
+    giraffe.add(pattern[i])
+
+giraffe.setDepth(10)
+paper.add(giraffe)
+
+cat.moveTo(110, 180)
+cat.setDepth(10)
+paper.add(cat)
+
+
+timeDelay = 0.1
+
+
+for i in range(10):
+    cat.move(-10 - i*10, 0)
+
+    sun.move(10+i*15, 5*i)
+    sleep(timeDelay*2)
+
+    giraffe.move(10+i*5, +5*i)
+    sleep(timeDelay)
